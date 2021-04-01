6c6ad4d7639a9561d54b48d4975dd732f4316d4f
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-04-01 01:13:22 -0300

    Incluindo filtro de vagas

--- src/main/java/com/uff/pareaqui/controller/VagaController.java
+++ src/main/java/com/uff/pareaqui/controller/VagaController.java
@@ -7,8 +7,8 @@ import com.uff.pareaqui.service.VagaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,10 +18,19 @@ public class VagaController {
     VagaService service;
 
     @GetMapping
-    public Map<String, Object> getVagasFiltradas(@RequestBody Map<String, ?> input) {
+    public Map<String, Object> getVagasFiltradas(@RequestParam Map<String, ?> input) {
         Map<String, Object> ret = new HashMap<String, Object>();
         try {
-            ret.put("data", service.filtraVagas(Long.parseLong(String.valueOf((Object) input.get("vaga_id")))));
+            ret.put("data",
+                    service.filtraVagas(String.valueOf((Object) input.get("menor_preco")),
+                            String.valueOf((Object) input.get("maior_preco")),
+                            String.valueOf((Object) input.get("tipos_escolhidos")),
+                            String.valueOf((Object) input.get("tamanhos_escolhidos")),
+                            String.valueOf((Object) input.get("estacionamento")).equals("1"),
+                            String.valueOf((Object) input.get("sem_acidentes")).equals("1"),
+                            String.valueOf((Object) input.get("sem_flanelinha")).equals("1"),
+                            String.valueOf((Object) input.get("order_preco")).equals("1"),
+                            String.valueOf((Object) input.get("order_preco_asc")).equals("1")));
             ret.put("success", true);
             ret.put("message", "Vagas recuperadas com sucesso.");
         } catch (Exception exception) {
--- src/main/java/com/uff/pareaqui/service/VagaService.java
+++ src/main/java/com/uff/pareaqui/service/VagaService.java
@@ -1,5 +1,6 @@
 package com.uff.pareaqui.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,10 +11,11 @@ import com.uff.pareaqui.repository.VagaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 @Service
+@RestController
 public class VagaService {
 
     @Autowired
@@ -32,9 +34,52 @@ public class VagaService {
         return repository.save(vaga);
     }
 
-    public List<Map<String, Object>> filtraVagas(Long vagaId) {
-        return jdbcTemplate.queryForList("SELECT * FROM vagas WHERE id = :id",
-                new MapSqlParameterSource().addValue("id", vagaId));
+    public List<Map<String, Object>> filtraVagas(String menorPreco, String maiorPreco, String tiposEscolhidos,
+            String tamanhosEscolhidos, boolean estacionamento, boolean semAcidentes, boolean semFlanelinha,
+            boolean orderPreco, boolean orderPrecoAsc) {
+        String baseFrom = "SELECT v.id AS 'vaga_id',v.identificacao AS 'vaga_identificacao',v.preco AS 'vaga_preco',vtipo.id AS 'vaga_tipo_id' ,vtipo.tipo AS 'vaga_tipo',vtipo.img AS 'tipo_img',vtamanho.id AS 'vaga_tamanho_id',vtamanho.tamanho AS 'vaga_tamanho', vagas_todas.estacionamento AS 'estacionamento',vagas_todas.rua AS 'rua',vagas_todas.numero AS 'numero',vagas_todas.cidade AS 'cidade',vagas_todas.complemento AS 'complemento',vagas_todas.bairro AS 'bairro',vagas_todas.estado AS 'estado',vagas_todas.pais AS 'pais' FROM vagas v INNER JOIN ( SELECT e.id AS 'estacionamento',e.rua AS 'rua',e.complemento AS 'complemento',e.cidade AS 'cidade',e.numero AS 'numero',e.bairro AS 'bairro',e.estado AS 'estado',e.pais AS 'pais',ev.vaga_id AS 'vaga_id' FROM estacionamento_vagas ev INNER JOIN estacionamentos e ON (ev.estacionamento_id=e.id) UNION SELECT 0 AS 'estacionamento',rv.rua AS 'rua',rv.complemento AS 'complemento',rv.numero AS 'numero',rv.cidade AS 'cidade',rv.bairro AS 'bairro',rv.estado AS 'estado',rv.pais AS 'pais',rv.vaga_id AS 'vaga_id' FROM rua_vagas rv ) vagas_todas ON (v.id=vagas_todas.vaga_id) INNER JOIN vaga_tipos vtipo ON (v.tipo_id=vtipo.id) INNER JOIN vaga_tamanhos vtamanho ON (v.tamanho_id=vtamanho.id)";
+
+        String where = "";
+
+        if (estacionamento) {
+            where += (where == "" ? " WHERE " : " AND ") + "(consulta_vagas.estacionamento <> 0)";
+        }
+
+        if (semAcidentes) {
+            where += (where == "" ? " WHERE " : " AND ")
+                    + "(consulta_vagas.vaga_id NOT IN (SELECT DISTINCT vaga_id FROM vaga_acidentes) AND consulta_vagas.estacionamento NOT IN (SELECT DISTINCT estacionamento_vagas.estacionamento_id FROM vaga_acidentes INNER JOIN vagas ON (vaga_acidentes.vaga_id=vagas.id) INNER JOIN estacionamento_vagas ON (estacionamento_vagas.vaga_id=vagas.id)) )";
+        }
+
+        if (menorPreco != null && menorPreco != "null" && maiorPreco != null && maiorPreco != "null") {
+            where += (where == "" ? " WHERE " : " AND ") + "(consulta_vagas.vaga_preco BETWEEN " + menorPreco + " AND "
+                    + maiorPreco + ")";
+        }
+
+        if (tiposEscolhidos != null && tiposEscolhidos != "null") {
+            where += (where == "" ? " WHERE " : " AND ") + "(consulta_vagas.vaga_tipo_id IN (" + tiposEscolhidos + "))";
+        }
+
+        if (tamanhosEscolhidos != null && tamanhosEscolhidos != "null") {
+            where += (where == "" ? " WHERE " : " AND ") + "(consulta_vagas.vaga_tamanho_id IN (" + tamanhosEscolhidos
+                    + "))";
+        }
+
+        if (semFlanelinha) {
+            where += (where == "" ? " WHERE " : " AND ")
+                    + "(CONCAT(consulta_vagas.pais,consulta_vagas.estado,consulta_vagas.cidade,consulta_vagas.bairro,consulta_vagas.complemento,consulta_vagas.rua,consulta_vagas.numero) NOT IN (SELECT DISTINCT CONCAT(pais,estado,cidade,bairro,complemento,rua,numero) FROM flanelinha_denuncias))";
+        }
+
+        String order = "";
+        if (orderPreco) {
+            order += (order == "" ? " ORDER BY " : " AND ") + " consulta_vagas.vaga_preco ";
+            if (!orderPrecoAsc) {
+                order += " DESC ";
+            }
+        }
+
+        String sql = "SELECT * FROM (" + baseFrom + ") consulta_vagas" + where + order;
+
+        return jdbcTemplate.queryForList(sql);
     }
 
     public Vaga getVaga(Long id) {
