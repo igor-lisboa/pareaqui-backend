6908cb2c08ef6e3cf953862fa7fd1c73009c0b0d
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-04-22 13:27:05 -0300

    Ajustando documentacao

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Para rodar o projeto:
 1. Copie o `.env.example` para o `.env` [`cp .env.example .env`] e defina valores para seu ambiente
 2. Rode o spring`mvn spring-boot:run`
+* Documentação Postman:
+    * https://documenter.getpostman.com/view/15046943/TzJvfdJj
--- src/main/java/com/uff/pareaqui/controller/HomeController.java
+++ src/main/java/com/uff/pareaqui/controller/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
         ret.put("success", true);
         ret.put("message", "Olá a documentação está no POSTMAN");
         Map<String, Object> documentacao = new HashMap<String, Object>();
-        documentacao.put("postman", "https://documenter.getpostman.com/view/13081554/TzCMe8PM");
+        documentacao.put("postman", "https://documenter.getpostman.com/view/15046943/TzJvfdJj");
         documentacao.put("github", "https://github.com/igor-lisboa/pareaqui-backend");
         ret.put("data", documentacao);
         return ret;
